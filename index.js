c6969cce7b77f3dcf2fdbface2a1b62d2deddbb1
Jose Soto <jsoto7087@gmail.com>
2022-07-01 13:00:11 -0400

    wages EarnedOnDare not passing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Your Code Here */
-
+// https://github.com/jsoto007/phase-1-context-lab.git
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +21,57 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array) {
+    const employeeInfo = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employeeInfo;
+
+}
+
+function createEmployeeRecords(array) {
+    return array.map(function (row) {
+        return createEmployeeRecord(row);
+    })
+}
+
+function createTimeInEvent(array) {
+    let [date, hour] = array.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+
+    })
+    return this;
+}
+
+function createTimeOutEvent(array) {
+    let [date, hour] = array.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(date, targetDate = "2044-03-15") {
+    const foundTimeIn = this.timeInEvents.find(object => object.date === targetDate);
+    const foundTimeOut = this.timeOutEvents.find(object => object.date === targetDate);
+    let workedHours = foundTimeOut.hour - foundTimeIn.hour;
+    return (workedHours / 100);
+}
+
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate(date) * this.payPerHour
+}
